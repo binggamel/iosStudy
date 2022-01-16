252f7f8e901f94931ceffd921c45b2836e2c0388
binggamel <kcyeun1203@naver.com>
2022-01-16 23:32:02 +0900

    first commit

--- iosStudy/ViewController.swift
+++ iosStudy/ViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue = 0
 
+    @IBOutlet weak var priceLable: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        refresh()
     }
 
-
+    
+    @IBAction func BtnHello(_ sender: Any) {
+        let message = "이 사진의 가격은 ₩\(currentValue)입니다"
+        
+        let alert = UIAlertController(title: "구매하시겠습니까?", message: message, preferredStyle: .alert)
+        let action01 = UIAlertAction(title: "YES", style: .default, handler: {
+            action01 in self.refresh()
+        })
+        
+        let action02 = UIAlertAction(title: "NO", style: .default, handler: {
+            action02 in self.refresh()
+        })
+        
+        alert.addAction(action01)
+        alert.addAction(action02)
+        
+        present(alert, animated: true, completion: nil)
+   
+    }
+    
+    func refresh() {
+        let randonRrice = arc4random_uniform(100000) + 1
+        currentValue = Int(randonRrice)
+        priceLable.text = "\(currentValue)"
+    }
+    
+    
 }
 
